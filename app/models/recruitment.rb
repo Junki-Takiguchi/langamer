5e518704b4c840c7e7bfa087045ff06d773c9ac6
Junki-Takiguchi <junki.takiguchi.2129@outlook.com>
2022-01-27 21:32:36 +0900

    setting association on models

--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -1,4 +1,5 @@
 class Recruitment < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 30}
+  belongs_to :user
+  validates :name, presence: true, length: { maximum: 32}
   validates :email, presence: true, length: { maximum: 1000}
 end
