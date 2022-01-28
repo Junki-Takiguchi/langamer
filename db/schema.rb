560fc87e665c84a119fa9c1d2a81d33339227671
Junki-Takiguchi <junki.takiguchi.2129@outlook.com>
2022-01-28 19:37:11 +0900

    created game_relations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_28_101937) do
+ActiveRecord::Schema.define(version: 2022_01_28_103318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2022_01_28_101937) do
   create_table "game_platforms", force: :cascade do |t|
     t.string "name"
     t.text "game_platform_picture"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "game_relations", force: :cascade do |t|
+    t.integer "game_id"
+    t.integer "game_platform_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
