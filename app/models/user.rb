74d3434288b002254b8a0d8860e1f1f79f9ddcca
Junki-Takiguchi <junki.takiguchi.2129@outlook.com>
2022-01-28 21:47:15 +0900

    setting association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :recruitments, dependent: :destroy
   has_one :user_detail, dependent: :destroy
   has_one :game_account, dependent: :destroy
+  has_many :from_applicant_user, foreign_key: 'from_applicant', class_name: 'FriendRelation', dependent: :destroy
+  has_many :to_target_user, foreign_key: 'to_target', class_name: 'FriendRelation', dependent: :destroy
+  has_many :my_friend, through: :from_applicant_user, source: :to_target
+  has_many :friend, through: :to_target_user, source: :from_applicant
 
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable,
