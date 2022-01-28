fb52faa94a249695e9dba7d7ff90121e0c9f0699
Junki-Takiguchi <junki.takiguchi.2129@outlook.com>
2022-01-28 20:34:52 +0900

    removed integer columns on GameRelations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_28_110757) do
+ActiveRecord::Schema.define(version: 2022_01_28_112821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,9 +39,6 @@ ActiveRecord::Schema.define(version: 2022_01_28_110757) do
   end
 
   create_table "game_relations", force: :cascade do |t|
-    t.integer "game_id"
-    t.integer "game_platform_id"
-    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
