82eb1ed4758666b76362790bd591db715c30f967
Junki-Takiguchi <junki.takiguchi.2129@outlook.com>
2022-02-02 23:57:59 +0900

    added through option on game model and game_platform model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :user
   has_many :game_relations, dependent: :destroy
+  has_many :game_relation_game_platforms, through: :game_relations, source: :game_platform
   validates :name, presence: true, length: { maximum: 64}
   validates :content, length: { maximum: 255}
 end
