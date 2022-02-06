b54a8f5fe73d49c0c1483bfe5c5a87b23407a713
Junki-Takiguchi <junki.takiguchi.2129@outlook.com>
2022-02-06 19:24:31 +0900

    updated enum on models

--- app/models/game_account.rb
+++ app/models/game_account.rb
@@ -14,14 +14,14 @@ class GameAccount < ApplicationRecord
     validates :other_id
   end
 
-  enum discord_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }
-  enum steam_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }
-  enum origin_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }
-  enum riot_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }
-  enum psn_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }
-  enum xbox_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }
-  enum nintendo_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }
-  enum other_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }
-  enum twitter_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }
+  enum discord_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }, _prefix: true
+  enum steam_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }, _prefix: true
+  enum origin_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }, _prefix: true
+  enum riot_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }, _prefix: true
+  enum psn_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }, _prefix: true
+  enum xbox_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }, _prefix: true
+  enum nintendo_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }, _prefix: true
+  enum other_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }, _prefix: true
+  enum twitter_id_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }, _prefix: true
 
 end
--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -11,10 +11,10 @@ class UserDetail < ApplicationRecord
   validates :native_country, length: { maximum: 64}
 
   enum gender: { "男性": 0, "女性": 1, "その他": 2 }
-  enum living_country_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }
-  enum native_country_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }
-  enum date_of_birth_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }
-  enum gender_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }
+  enum living_country_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }, _prefix: true
+  enum native_country_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }, _prefix: true
+  enum date_of_birth_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }, _prefix: true
+  enum gender_status: { "公開": 0, "非公開": 1, "フレンドのみ公開": 2 }, _prefix: true
 
 
   def country_name(country_code)
