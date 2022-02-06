5ae3637eed1b33ee386274e143c7a5514c2cb167
Junki-Takiguchi <junki.takiguchi.2129@outlook.com>
2022-02-06 17:21:41 +0900

    created migration file for add status calumn

---
+++ db/migrate/20220206075725_add_columns_to_user_detail.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUserDetail < ActiveRecord::Migration[6.0]
+  def change
+    add_column :user_details, :second_language_status, :integer, default: 1, null: false, limit: 2
+    add_column :user_details, :living_country_status, :integer, default: 1, null: false, limit: 2
+    add_column :user_details, :native_country_status, :integer, default: 1, null: false, limit: 2
+    add_column :user_details, :date_of_birth_status, :integer, default: 1, null: false, limit: 2
+    add_column :user_details, :gender_status, :integer, default: 1, null: false, limit: 2
+  end
+end
