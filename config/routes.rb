630a7e94834d9f766efde50e7dc8aee7d7d049ab
Junki-Takiguchi <junki.takiguchi.2129@outlook.com>
2022-02-09 13:28:47 +0900

    setting chat_room and message route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   resources :recruitments
   resources :users, only: [:index, :update, :show]
   resources :friend_relations, only: [:create, :update, :destroy]
+  resources :chat_rooms do
+    resources :messages
+  end
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
