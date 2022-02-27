7f7978142f4c97a2275ce0dc79cbc954fb7c2265
Junki-Takiguchi <junki.takiguchi.2129@outlook.com>
2022-02-28 00:16:12 +0900

    updated sessions.new page

--- app/models/message.rb
+++ app/models/message.rb
@@ -10,14 +10,24 @@ class Message < ApplicationRecord
     comment.update(read: "既読")
   end
 
-  def relative_post_date(created_at)
-    sec = (Time.zone.now - created_at).round
-    days = sec / (60 * 60 * 24)
-      return "#{days}日前" unless days.zero?
-    hours = sec / (60 * 60)
-      return "#{hours}時間前" unless hours.zero?
-    min = sec / 60
-      return "#{min}分前" unless min.zero?
-    "#{sec}秒前"
+  def relative_post_date(updated_at, locale)
+    sec = (Time.zone.now - updated_at).round
+    if locale == "ja"
+      days = sec / (60 * 60 * 24)
+        return "#{days}日前" unless days.zero?
+      hours = sec / (60 * 60)
+        return "#{hours}時間前" unless hours.zero?
+      min = sec / 60
+        return "#{min}分前" unless min.zero?
+      "#{sec}秒前"
+    else
+      days = sec / (60 * 60 * 24)
+        return "#{days}days ago" unless days.zero?
+      hours = sec / (60 * 60)
+        return "#{hours}hours ago " unless hours.zero?
+      min = sec / 60
+        return "#{min}minutes ago" unless min.zero?
+      "#{sec}seconds ago"
+    end
   end
 end
