6395b2bccad6c6a4853fdcb7ac8175b10fad2e97
Junki-Takiguchi <junki.takiguchi.2129@outlook.com>
2022-02-27 20:04:54 +0900

    updated users show and recruitment show page for I18n

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   skip_before_action :authenticate_user!, only: [:new, :create]
+  before_action :reset_gender, only: [:create, :update]
 
   # GET /resource/sign_up
   def new
@@ -31,6 +32,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def destroy
     super
   end
+
+  private
+    def reset_gender
+      unless params[:locale] == "ja"
+        if params[:user][:user_detail_attributes][:gender] == "Male"
+          params[:user][:user_detail_attributes][:gender] = 0
+        elsif params[:user][:user_detail_attributes][:gender] == "Female"
+          params[:user][:user_detail_attributes][:gender] = 1
+        else
+          params[:user][:user_detail_attributes][:gender] = 2
+        end
+      end
+    end
 
 
   protected
