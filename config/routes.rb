3c6441789a833682fd628884ad213729af78d3c2
Junki-Takiguchi <junki.takiguchi.2129@outlook.com>
2022-02-27 21:28:18 +0900

    updated _form.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,22 @@
 Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-scope "(:locale)", locale: /#{I18n.available_locales.map(&:to_s).join("|")}/ do
-  root 'recruitments#index'
-  devise_for :users, controllers: {
-        registrations: 'users/registrations'
-  }
-  resources :recruitments do
-    collection do
-      match 'search', to: 'recruitments#search', via: [:get, :post]
+  scope "(:locale)", locale: /#{I18n.available_locales.map(&:to_s).join("|")}/ do
+    root 'recruitments#index'
+    devise_for :users, controllers: {
+          registrations: 'users/registrations'
+    }
+    resources :recruitments do
+      collection do
+        match 'search', to: 'recruitments#search', via: [:get, :post]
+      end
     end
-  end
-
-  resources :users, only: [:update, :show]
-  resources :friend_relations, only: [:index, :create, :update, :destroy]
-  patch 'friend_relation/:id', to: 'friend_relations#index'
-  resources :chat_rooms, only: [:index]
-
-  get 'message/:id', to: 'messages#show', as: 'chat_message'
-  resources :messages, only: [:get, :create]
-
+    resources :users, only: [:update, :show]
+    resources :friend_relations, only: [:index, :create, :update, :destroy]
+    patch 'friend_relation/:id', to: 'friend_relations#index'
+    resources :chat_rooms, only: [:index]
+    get 'message/:id', to: 'messages#show', as: 'chat_message'
+    resources :messages, only: [:get, :create]
   end
 
   if Rails.env.development?
