cdbc955a845c89ca4d08623eb2b77ebb2d230f9e
Junki-Takiguchi <junki.takiguchi.2129@outlook.com>
2022-03-24 04:09:30 +0900

    updated route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,7 @@ Rails.application.routes.draw do
     resources :friend_relations, only: [:index, :create, :update, :destroy]
     patch 'friend_relation/:id', to: 'friend_relations#index'
     resources :chat_rooms, only: [:index]
-    get 'message/:id', to: 'messages#show', as: 'chat_message'
-    resources :messages, only: [:get, :create]
+    resources :messages, only: [:get, :create, :show]
   end
 
   if Rails.env.development?
